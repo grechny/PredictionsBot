1d31681c89e3d890df9a3fac23295a8adda32d91
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2026-05-22 11:50:08 +0200

    refactor: move league rules to kotlin

--- src/main/java/at/hrechny/predictionsbot/service/predictor/LeagueService.java
+++ src/main/java/at/hrechny/predictionsbot/service/predictor/LeagueService.java
@@ -6,16 +6,13 @@ import at.hrechny.predictionsbot.database.repository.CompetitionRepository;
 import at.hrechny.predictionsbot.database.repository.LeagueRepository;
 import at.hrechny.predictionsbot.database.repository.UserRepository;
 import at.hrechny.predictionsbot.exception.InputValidationException;
-import at.hrechny.predictionsbot.exception.LimitExceededException;
 import at.hrechny.predictionsbot.model.LeagueRequest;
 import at.hrechny.predictionsbot.model.LeagueResponse;
-import at.hrechny.predictionsbot.util.NameUtils;
 import jakarta.transaction.Transactional;
 import java.util.Set;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -23,17 +20,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LeagueService {
 
-  private static final int MAX_LEAGUES_PER_USER = 10;
-
   private final LeagueRepository leagueRepository;
   private final UserRepository userRepository;
   private final CompetitionRepository competitionRepository;
+  private final LeagueRules leagueRules;
 
   @Transactional
   public LeagueResponse create(Long userId, LeagueRequest leagueRequest) {
     log.info("Creating new league {} by user {}", leagueRequest.getName(), userId);
 
-    validateName(leagueRequest.getName());
+    leagueRules.validateName(leagueRequest.getName());
     var adminUser = getUser(userId);
     var competitions = competitionRepository.findAll().stream()
         .filter(competitionEntity -> leagueRequest.getCompetitions().contains(competitionEntity.getId()))
@@ -54,9 +50,7 @@ public class LeagueService {
     var adminUser = getUser(userId);
     var league = leagueRepository.findById(leagueId).orElseThrow();
 
-    if (!league.getAdminUser().equals(adminUser)) {
-      throw new InputValidationException("User is not the admin of this league and cannot update it");
-    }
+    leagueRules.ensureLeagueAdmin(league.getAdminUser().equals(adminUser));
 
     return null;
   }
@@ -92,9 +86,7 @@ public class LeagueService {
     }
 
     var user = userRepository.findById(userId).orElseThrow();
-    if (user.getLeagues().size() > MAX_LEAGUES_PER_USER) {
-      throw new LimitExceededException("User is already a member of another league");
-    }
+    leagueRules.ensureLeagueLimit(user.getLeagues().size());
 
     leagueEntity.getUsers().add(user);
     leagueRepository.save(leagueEntity);
@@ -102,23 +94,9 @@ public class LeagueService {
     return leagueEntity;
   }
 
-  private void validateName(String leagueName) throws InputValidationException {
-    log.info("Validating league name {}", leagueName);
-
-    if (StringUtils.isBlank(leagueName)) {
-      throw new InputValidationException("League name cannot be empty");
-    }
-
-    if (!NameUtils.isValid(leagueName)) {
-      throw new InputValidationException("League name should be 3-20 characters long and contain only valid characters");
-    }
-  }
-
   private UserEntity getUser(Long userId) {
     var user = userRepository.findById(userId).orElseThrow();
-    if (user.getLeagues().size() > MAX_LEAGUES_PER_USER) {
-      throw new LimitExceededException("User is already a member of another league");
-    }
+    leagueRules.ensureLeagueLimit(user.getLeagues().size());
     return user;
   }
 
---
+++ src/main/kotlin/at/hrechny/predictionsbot/service/predictor/LeagueRules.kt
@@ -0,0 +1,39 @@
+package at.hrechny.predictionsbot.service.predictor
+
+import at.hrechny.predictionsbot.exception.InputValidationException
+import at.hrechny.predictionsbot.exception.LimitExceededException
+import at.hrechny.predictionsbot.util.NameUtils
+import jakarta.inject.Singleton
+import org.apache.commons.lang3.StringUtils
+import org.springframework.stereotype.Component
+
+@Component
+@Singleton
+class LeagueRules {
+
+    fun validateName(leagueName: String?) {
+        if (StringUtils.isBlank(leagueName)) {
+            throw InputValidationException("League name cannot be empty")
+        }
+
+        if (!NameUtils.isValid(leagueName)) {
+            throw InputValidationException("League name should be 3-20 characters long and contain only valid characters")
+        }
+    }
+
+    fun ensureLeagueLimit(leagueCount: Int) {
+        if (leagueCount > MAX_LEAGUES_PER_USER) {
+            throw LimitExceededException("User is already a member of another league")
+        }
+    }
+
+    fun ensureLeagueAdmin(admin: Boolean) {
+        if (!admin) {
+            throw InputValidationException("User is not the admin of this league and cannot update it")
+        }
+    }
+
+    private companion object {
+        const val MAX_LEAGUES_PER_USER = 10
+    }
+}
