b4b4e64f2ee63e7d85af090555e3830cc72fc527
Aliaksandr Hrechny <fotonn.mail@gmail.com>
2026-05-30 19:21:35 +0200

    fix(03): simplify exception report dependency

--- src/main/kotlin/at/hrechny/predictionsbot/exception/interceptor/ExceptionInterceptor.kt
+++ src/main/kotlin/at/hrechny/predictionsbot/exception/interceptor/ExceptionInterceptor.kt
@@ -3,20 +3,19 @@ package at.hrechny.predictionsbot.exception.interceptor
 import at.hrechny.predictionsbot.service.telegram.TelegramService
 import io.micronaut.aop.MethodInterceptor
 import io.micronaut.aop.MethodInvocationContext
-import io.micronaut.context.BeanProvider
 import jakarta.inject.Singleton
 import org.slf4j.LoggerFactory
 
 @Singleton
 class ExceptionInterceptor(
-    private val telegramServiceProvider: BeanProvider<TelegramService>,
+    private val telegramService: TelegramService,
 ) : MethodInterceptor<Any, Any> {
     override fun intercept(context: MethodInvocationContext<Any, Any>): Any? {
         try {
             return context.proceed()
         } catch (exception: RuntimeException) {
             try {
-                telegramServiceProvider.get().sendErrorReport(exception)
+                telegramService.sendErrorReport(exception)
             } catch (reportException: RuntimeException) {
                 log.error("Failed to send error report", reportException)
             }
